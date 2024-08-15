87debf6392e8e10148a295f07488dc1daa8bd4e4
Malpizarr <m.alpizar@hotmail.com>
2024-08-15 11:28:44 -0600

    .

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'cuidados-paliativos/portal',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '0a3d2bb8e9d44055def2f010325a436f92d09a71',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'cuidados-paliativos/portal' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '0a3d2bb8e9d44055def2f010325a436f92d09a71',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
